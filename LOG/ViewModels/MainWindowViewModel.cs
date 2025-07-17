2b5fc621be38dab3dff125db9c0a28641551890f
FS <1393923267@qq.com>
2025-07-17 19:56:11 +0800

    change basic style

--- LOG/ViewModels/MainWindowViewModel.cs
+++ LOG/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace LOG.ViewModels;
+﻿using Avalonia.Controls;
+
+namespace LOG.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public string Greeting { get; } = "欢迎访问 23号 终端!";
+
+    public string[] Options { get; } =
+    {
+        "[a]",
+        "[b]",
+        "[c]"
+    };
+    
+    
+    
+    
 }
